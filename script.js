5c5cf3584b712ec76b118706ce9ef06080d3c064
cassiovieira <125905694+cassiovieiraa@users.noreply.github.com>
2023-03-22 14:12:48 -0300

    Teste

---
+++ script.js
@@ -0,0 +1,26 @@
+function gerar(){
+    var num = document.getElementById('inum')
+    var tab = document.getElementById('seltab')
+   
+    if(num.value.length == 0){
+         window.alert('[ERRO] Por favor digite um número')
+    }else{
+        var n = Number(num.value)
+        var c = 1
+        tab.innerHTML = ''
+        while(c <= 10){
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            tab.appendChild(item)
+            c++
+        }
+
+    }
+
+
+
+
+
+
+
+}
---
+++ script2.js
@@ -0,0 +1,19 @@
+function gerar(){
+    var num = document.getElementById('inum')
+    var tab = document.getElementById('seltab')
+    
+
+    if(num.value.length == 0){
+        window.alert('Por favor digite um número!')
+    }else{
+        var n = Number(num.value)
+        tab.innerHTML = ''
+        for(var c = 1;c<=10; c++){
+            var item = document.createElement('option')
+            item.text = `${n} x ${c} = ${n*c}`
+            item.value = `tab${c}` //para dar valor em outras linguagens como php
+            tab.appendChild(item)
+
+        }
+    }
+}
